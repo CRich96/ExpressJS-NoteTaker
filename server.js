f99258104347ee1eed1057a8b37de189509058a5
CRich96 <you@example.comchris575502@icloud.com>
2023-10-19 15:51:54 -0500

    add server js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const PORT = 3001;
+const reviews = require('./db/db.json');
+
+const app = express();
+
+// Middleware for parsing application/json and urlencoded data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
