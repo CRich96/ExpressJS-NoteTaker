37d9b0df59a4166861eefdf7af69eb0a80573083
CRich96 <you@example.comchris575502@icloud.com>
2023-10-23 08:00:15 -0500

    master

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ require("./routes/api-routes")(app);
 
 
 // Starts the server to begin listening
-app.listen(PORT, function() {
+app.listen(process.env.PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
